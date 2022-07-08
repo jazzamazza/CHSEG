e12611f92e649de4fa86f281e4a8c241d5279fcb
jemma <jemmasundelson@gmail.com>
2022-07-08 10:24:46 +0200

    test cloud comapre

--- set_up.py
+++ set_up.py
@@ -6,9 +6,15 @@ from sklearn.cluster import KMeans
 import open3d as o3d
 from mpl_toolkits import mplot3d
 from datetime import datetime
+from yaml import load
+import laspy as lp
 from scipy.spatial.distance import cdist
 from sklearn.metrics import silhouette_samples, silhouette_score
 import matplotlib.cm as cm
+from sklearn_extra.cluster import KMedoids #pip install https://github.com/scikit-learn-contrib/scikit-learn-extra/archive/master.zip
+#from sklearn_extra.cluster import KMedians
+import sklearn_extensions as ske
+
 
 # Clustering class with various clustering methods
 class Clustering:
@@ -36,13 +42,63 @@ class Clustering:
                label='centroids'
           )
           #plt.legend()
-          plt.title('Two clusters of data')
+          plt.title('Two clusters of data: K-means')
           plt.savefig('k_means_clusters.png') 
           plt.show()
+     
+     def kMediods(self, k):
+          x = self.pcd
+          
+          print("\n------------------k means---------------------")
+          kmediods = KMedoids(n_clusters=k, n_init=10) # number of clusters (k)
+          kmediods.fit(x) # apply k means to dataset
+          
+          # Visualise K-Means
+          y_km = KMedoids.predict(x)
+          centroids = KMedoids.cluster_centers_
+          unique_labels = np.unique(y_km)
 
-def silhouette(self):
+          for i in unique_labels:
+               plt.scatter(x[y_km == i , 0] , x[y_km == i , 1] , label = i, marker='o', picker=True)
+          plt.scatter(
+               centroids[:, 0], centroids[:, 1],
+               s=100, marker='*',
+               c='red', edgecolor='black',
+               label='centroids'
+          )
+          #plt.legend()
+          plt.title('Two clusters of data: K-mediods')
+          plt.savefig('k_means_clusters.png') 
+          plt.show()
+     
+     # def kMeddians(self, k):
+     #      x = self.pcd
+          
+     #      print("\n------------------k means---------------------")
+     #      kmediods = KMedians(n_clusters=k, n_init=10) # number of clusters (k)
+     #      kmediods.fit(x) # apply k means to dataset
+          
+     #      # Visualise K-Means
+     #      y_km = KMedoids.predict(x)
+     #      centroids = KMedoids.cluster_centers_
+     #      unique_labels = np.unique(y_km)
 
-          x = np.asarray(self.pcd)
+     #      for i in unique_labels:
+     #           plt.scatter(x[y_km == i , 0] , x[y_km == i , 1] , label = i, marker='o', picker=True)
+     #      plt.scatter(
+     #           centroids[:, 0], centroids[:, 1],
+     #           s=100, marker='*',
+     #           c='red', edgecolor='black',
+     #           label='centroids'
+     #      )
+     #      #plt.legend()
+     #      plt.title('Two clusters of data')
+     #      plt.savefig('k_means_clusters.png') 
+     #      plt.show()
+
+     def silhouette(self):
+    
+          x = self.pcd
      
           K = range(2,10)
           for k in K:
@@ -141,7 +197,6 @@ def silhouette(self):
 
           plt.show()
 
-
      #output
      # For n_clusters = 2 The average silhouette_score is : 0.3889045527426348
      # For n_clusters = 3 The average silhouette_score is : 0.3403178007585951
@@ -152,24 +207,11 @@ def silhouette(self):
      # For n_clusters = 8 The average silhouette_score is : 0.329213106052044
      # For n_clusters = 9 The average silhouette_score is : 0.31500958433214615
 
-# Tester method to test Clustering class and k_means algorithm          
-def testMethod():
-     X = np.array([[5,3],
-     [10,15],
-     [15,12],
-     [24,10],
-     [30,45],
-     [85,70],
-     [71,80],
-     [60,78],
-     [55,52],
-     [80,91],])
      
-     clustering = Clustering(X)
-     clustering.k_means_clustering(4)
+
 
 # Method to load and visualise a point cloud in a .npy file using open3d
-def loadPointCloud_npy():
+def loadPointCloud_npy(vis):
      print("\nLOAD NPY POINT CLOUD DATA")
 
      #load point cloud to numpy
@@ -189,8 +231,9 @@ def loadPointCloud_npy():
      print(pcd)
 
      # visualise point cloud
-     downpcd = pcd.voxel_down_sample(voxel_size=0.05) # downsample pcd
-     o3d.visualization.draw_geometries([downpcd])
+     downpcd = pcd.voxel_down_sample(voxel_size=0.5) # downsample pcd
+     if (vis):
+          o3d.visualization.draw_geometries([downpcd])
      
      pc_points = np.asarray(downpcd.points) # convert pcd points to np array
      pc_features = np.asarray(downpcd.normals) # convert pcd additional features to np array
@@ -203,20 +246,28 @@ def loadPointCloud_npy():
      
      return finalPCD
 
+def loadPointCloud_xyz(vis):
+     print("\nLOAD PLY CC POINT CLOUD DATA\n")
+     input = "/Users/A102178/Desktop/church_registered - Cloud.txt"
+
+     PointCloud = np.loadtxt(input)
+     pc = np.asarray(PointCloud)
+
+     print("size of point cloud is:", pc.size)
+    
+
+     return pc
+
 # Method to load and visualise a point cloud in a .ply file using open3d
-def loadPointCloud_ply():
+def loadPointCloud_ply(vis):
      print("\nLOAD PLY POINT CLOUD DATA")
 
+
      #load point cloud .ply file
-     path = "/Users/A102178/Desktop/church_registered.ply"
+     path = "/Users/A102178/Desktop/church_registeredD.ply"
      pcd = o3d.io.read_point_cloud(path)
      
      print(pcd)
-     #print(np.asarray(pcd.points))
-     #print("Has colours:", pcd.has_colors(), np.asarray(pcd.colors)[0])
-     #print("Has normals:", pcd.has_normals(), np.asarray(pcd.normals)[0])
-     #print("Has points:", pcd.has_points(), np.asarray(pcd.points)[0])
-     #print("Has covariances:", pcd.has_covariances())
      
      downpcd = pcd.voxel_down_sample(voxel_size=0.05)
      
@@ -229,28 +280,142 @@ def loadPointCloud_ply():
      print("Downsampled Point cloud size: ", pc.size)
      
      return pc
+    
+def loadPointCloud_las(vis):
+     print("\nLOAD LAS POINT CLOUD DATA\n")
+     
+     path = "/Users/A102178/Desktop/church_registered_cldCmp6.las"  #church_registered _cldCmp (1).las
+     pcd = lp.read(path)
+
+     print("All features:", list(pcd.point_format.dimension_names))
+     points = np.vstack((pcd.x, pcd.y, pcd.z)).transpose()            #lists x, y, z cooridnates 
+     print("points", points)
+     
+     print("Cloud Compare Features:", list(pcd.point_format.extra_dimension_names))
+     planarity = np.vstack(pcd['Planarity (0.049006)'])
+     intensity = np.vstack(pcd['NormalX'])
+     anisotropy = np.vstack(pcd['Anisotropy (0.049006)'])
+     Linearity = np.vstack(pcd['Linearity (0.049006)'])
+     SurfaceVariation = np.vstack(pcd['Surface variation (0.049006)'])
+     Omnivariance = np.vstack(pcd['Omnivariance (0.049006)'])
+     Eigenentropy = np.vstack(pcd['Eigenentropy (0.049006)'])
+     
+     # print("\nPoints", points)
+     # print("\nPlanarity:", planarity)
+     # print("\nIntensity:", intensity)
+     
+     # format using open3d
+     pc = o3d.geometry.PointCloud()
+     pc.points = o3d.utility.Vector3dVector(points)
+     zero = intensity # placeholder
+     arr = np.hstack((anisotropy, planarity, Eigenentropy))
+     arr1 = np.hstack((Linearity, SurfaceVariation, Omnivariance))
+     #arr2 = np.hstack(Eigenentropy, Omnivariance)
+     #cloudCompareFeatures = np.hstack((arr, zero)) # form a 3D vector to add to o3d pcd
+     #cloudCompareFeatures1 = np.hstack((arr1))
+     pc.normals = o3d.utility.Vector3dVector(arr) # store additional features (intensity & planarity) in pc.normals
+     pc.colors = o3d.utility.Vector3dVector(arr1)
+     print(pc)
+
+
+     #0visualise point cloud
+     downpcd = pc.voxel_down_sample(voxel_size=0.05) # downsample pc
+     if (vis): 
+          o3d.visualization.draw_geometries([downpcd])
+     
+     pc_points = np.asarray(downpcd.points) # convert pc points to np array
+     pc_features = np.asarray(downpcd.normals) # convert pc additional features to np array
+     pc_features1 = np.asarray(downpcd.colors)
+
+     print("pc_points", np.asarray(pc.points))
+     print("pc_features", np.asarray(pc.normals))
+
+     print("pc_points downsampled", pc_points)
+     print("pc_features downsampled", pc_features)
+
+     finalPCD = np.hstack((pc_points, pc_features, pc_features1)) # concatenate the 2 np arrays - ADDED FEATURES1 - SAYS MISSING VALUES FOR K-MEANS
+     print("Downsampled Point cloud size: ", finalPCD.size)  # pc_features1
+     print("0 is:", finalPCD[0])
+
+
+     #remove_nanFinal = finalPCD[np.logical_not(np.isnan(finalPCD))]
+     # remove_nanFinal = np.nan_to_num(finalPCD)
+     # final = np.hstack(remove_nanFinal)
+     
+     return finalPCD
 
 # Helper method to call method to load .ply and .npy point cloud files        
 def setup():
      pointCloud = loadPointCloud_npy()
-     #pointCloud = loadPointCloud_ply()
-     return pointCloud
+     pointCloud_ply = loadPointCloud_ply()
+     pointCloud_las = loadPointCloud_las()
+     pointCloud_xyz = loadPointCloud_xyz()
+     return pointCloud, pointCloud_las, pointCloud_ply, pointCloud_xyz
 
 # main method
-def main():
-    #testMethod() #this works
-    
+def main():  
     start_time = datetime.now()
     print("Start Time = ", start_time.strftime("%H:%M:%S"))
     
-    pointCloud = setup() # load point cloud and store in a numpy array
+    pointCloud, pointCloud_las, pointCloud_ply, pointCloud_xyz = setup() # load point cloud and store in a numpy array
     
     # Cluster the point cloud
     clustering = Clustering(pointCloud)
     clustering.k_means_clustering(15)
     
+    clustering_cldCmp = Clustering(pointCloud_las) #pointCloud_las
+    clustering_cldCmp.k_means_clustering()
+
+    clustering_cldCmpXYZ = Clustering(pointCloud_xyz)
+    clustering_cldCmpXYZ.k_means_clustering(15)
+    
     end_time = datetime.now()
     print("End Time = ", end_time.strftime("%H:%M:%S"))
+
+# Helper method to call method to load point cloud files  
+# Returns a PointCloud in a numpy array      
+def newSetup(option, vis):
+     if (option == "1"): pointCloud = loadPointCloud_npy(vis) # setup point cloud with raw features 
+     elif (option == "2"): pointCloud = loadPointCloud_ply(vis) # setup point cloud with Cloud Compare features
+     elif (option == "3"): pointCloud = loadPointCloud_xyz(vis)
+     elif (option == "4"): pointCloud = loadPointCloud_las(vis) # setup point cloud with PointNet++ features
+     return pointCloud
+
+# interactive application
+def application():
+     userInput = ""
+     while (userInput != "q"):
+          print("--------------Welcome---------------")
+          print("Type q to quit the application")
+          # Choose Point Cloud
+          userInput = input("\nChoose Point Cloud Input:"+
+                         "\n 1 : Point Cloud with Raw Features"+
+                         "\n 2 : Point Cloud with Cloud Compare Features"+
+                         "\n 3 : Point Cloud with Cloud Compare Features using a text file"+
+                         "\n 4 : Point Cloud with PointNet++ Features\n")
+          if (userInput == "q"): break
+          pcd_choice = userInput
+          
+          # Setup and visualise point cloud based on user input
+          userInput = input("\nVisualise Point Cloud (y/n)?")
+          if (userInput == "q"): break
+          if (userInput=="y"):
+               pointCloud = newSetup(pcd_choice, True)
+          else:
+               pointCloud = newSetup(pcd_choice, False)
+          clustering = Clustering(pointCloud)
+     
+          while (userInput != "r"):
+               # cluster point cloud    
+               userInput = input("\nChoose Clustering Method(s):"+
+                              "\n 0 : K-Means Clustering" +
+                              "\n 1 : Clustering Method 1"+
+                              "\n 2 : Clustering Method 2"+
+                              "\n 3 : Clustering Method 3"+
+                              "\n r : Restart the Application\n")
+               if (userInput == "q"): break
+               elif (userInput == "0"): clustering.k_means_clustering(15)
             
 if __name__=="__main__":
-    main()
+    application()
+    #main()
