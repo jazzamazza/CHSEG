c00ce72c0c46796b641f219f73bc66bbfde3b56d
Leah Gluckman <81581707+glclea001@users.noreply.github.com>
2022-07-18 15:24:42 +0200

    Update test_semseg.py

--- test_semseg.py
+++ test_semseg.py
@@ -20,15 +20,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = BASE_DIR
 sys.path.append(os.path.join(ROOT_DIR, 'models'))
 
-classes = ['ceiling', 'floor', 'wall', 'beam', 'column', 'window', 'door', 'table', 'chair', 'sofa', 'bookcase',
-           'board', 'clutter']
-class2label = {cls: i for i, cls in enumerate(classes)}
-seg_classes = class2label
-seg_label_to_cat = {}
-for i, cat in enumerate(seg_classes.keys()):
-    seg_label_to_cat[i] = cat
-
-
 def parse_args():
     '''PARAMETERS'''
     parser = argparse.ArgumentParser('Model')
@@ -36,9 +27,6 @@ def parse_args():
     parser.add_argument('--gpu', type=str, default='0', help='specify gpu device')
     parser.add_argument('--num_point', type=int, default=4096, help='point number [default: 4096]')
     parser.add_argument('--log_dir', type=str, required=True, help='experiment root')
-    parser.add_argument('--visual', action='store_true', default=False, help='visualize result [default: False]')
-    parser.add_argument('--test_area', type=int, default=5, help='area for testing, option: 1-6 [default: 5]')
-    parser.add_argument('--num_votes', type=int, default=3, help='aggregate segmentation scores with voting [default: 5]')
     return parser.parse_args()
 
 def main(args):
@@ -50,9 +38,6 @@ def main(args):
     os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
     experiment_dir = 'log/sem_seg/' + args.log_dir
     print("experiment dir:", experiment_dir)
-    visual_dir = experiment_dir + '/visual/'
-    visual_dir = Path(visual_dir)
-    visual_dir.mkdir(exist_ok=True)
 
     '''LOG'''
     args = parse_args()
@@ -71,8 +56,7 @@ def main(args):
     NUM_POINT = args.num_point
 
     root = "/content/drive/MyDrive/Thesis_Testing/PNET/Data"
-    TEST_DATASET_WHOLE_SCENE = ScannetDatasetWholeScene(root)
-    log_string("The number of test data is: %d" % len(TEST_DATASET_WHOLE_SCENE))
+    DATASET = ScannetDatasetWholeScene(root)
 
     '''MODEL LOADING'''
     print("DIRECTORY:",experiment_dir + '/logs')
@@ -85,15 +69,15 @@ def main(args):
     classifier = classifier.eval()
 
     with torch.no_grad():
-        scene_id = TEST_DATASET_WHOLE_SCENE.file_list
+        scene_id = DATASET.file_list
         scene_id = [x[:-4] for x in scene_id]
-        num_batches = len(TEST_DATASET_WHOLE_SCENE)
+        num_batches = len(DATASET)
 
         log_string('---- EVALUATION WHOLE SCENE----')
 
-        whole_scene_data = TEST_DATASET_WHOLE_SCENE.scene_points_list[0]    #get all data
-        whole_scene_label = TEST_DATASET_WHOLE_SCENE.semantic_labels_list[0]    #get all labels 
-        scene_data = TEST_DATASET_WHOLE_SCENE[0]
+        whole_scene_data = DATASET.scene_points_list[0]    #get all data
+        whole_scene_label = DATASET.semantic_labels_list[0]    #get all labels 
+        scene_data = DATASET[0]
         print("outside")
         num_blocks = scene_data.shape[0]
         s_batch_num = (num_blocks + BATCH_SIZE - 1) // BATCH_SIZE
@@ -124,10 +108,12 @@ def main(args):
         
         finalPCD = np.hstack((final_xyz_list, final_feat_list))
         print("finalPCD[0]:", finalPCD[0])
+
+        finalPCD_new = finalPCD.reshape(-1,2)
         print("*********************************")
 
-        clustering = Clustering(finalPCD, "3")
-        clustering.k_means_clustering_faiss(15)
+        clustering = Clustering(finalPCD_new, "3")
+        clustering.k_means_clustering_faiss(3)
 
         print("Done!")
 
