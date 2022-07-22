be35d5bab726b6bccff05c72452eb5ebc80d59ab
Jared May <jared@jaredmay.co.za>
2022-07-22 12:30:29 +0200

    pptk added

--- visPCD.py
+++ visPCD.py
@@ -15,11 +15,26 @@ import pptk
 
 print('pptk test')
 
+
 # x = np.random.rand(100, 3)
 # v = pptk.viewer(x)
 # v.set(point_size=0.01)
 
-xyz = pptk.rand(100, 3)
-rgb = pptk.rand(100, 3)
-v = pptk.viewer(xyz, rgb)
-v.set(point_size=0.005)
+# xyz = pptk.rand(100, 3)
+# rgb = pptk.rand(100, 3)
+# v = pptk.viewer(xyz, rgb)
+# v.set(point_size=0.005)
+
+# pcd = inputPath = "./Data/church_registered.npy" #path to point cloud file
+#      pointCloud = np.load(inputPath)
+     
+#load point cloud .ply file
+path = "/Volumes/Space120/Datasets/UCT/Data/church_registered_downsampled_0.05.ply"
+#path = "./Data/church_registered_downsampled_0.5.ply"
+pcd = o3d.io.read_point_cloud(path)
+print(pcd)
+
+pcd_npy = np.asarray(pcd.points)
+
+view = pptk.viewer(pcd_npy)
+view.set(point_size=0.005)
