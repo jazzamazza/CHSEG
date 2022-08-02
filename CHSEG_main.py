2219c9c005eb0299d27f27a48534b5a387de5cf5
Leah Gluckman <81581707+glclea001@users.noreply.github.com>
2022-08-02 14:47:10 +0200

    Update CHSEG_main.py

--- CHSEG_main.py
+++ CHSEG_main.py
@@ -1,7 +1,6 @@
 from Clustering import Clustering
 from PointCloudLoader import PointCloudLoader
 from tkinter import filedialog as fd
-from LeahClustering import LeahClustering
 
 def load(vis):
     file_types = [('Point Cloud Files','*.ply *.npy *.las *.xyz *.pcd')]
@@ -38,7 +37,7 @@ def load(vis):
 # Returns a PointCloud in a numpy array      
 def setup(option, vis, ds, dsSize):
     #SET PATH
-    file_path = "Data\church_registered.npy"
+    file_path = "/content/drive/Shareddrives/Leah_Thesis/Data/church_registered.npy"
     pc_loader = PointCloudLoader(file_path)
     if (option == "1"): 
         pointCloud, pcd_with_truths = pc_loader.load_point_cloud_npy(vis, ds, dsSize) # setup point cloud with raw features 
@@ -85,22 +84,21 @@ def application():
 
           pointCloud, pcd_with_truths = setup(pcd_choice,vis, ds, dsSize)
           clustering = Clustering(pointCloud, pcd_with_truths , pcd_choice)
-          leah = LeahClustering(pointCloud, pcd_with_truths , pcd_choice)
      
           while (userInput != "r"):
                # cluster point cloud    
                userInput = input("\nChoose Clustering Method(s):"+
                               "\n 0 : K-Means Clustering" +
-                              "\n 1 : Clustering Method 1"+
-                              "\n 2 : Clustering Method 2"+
-                              "\n 3 : Clustering Method 3"+
+                              "\n 1 : DBSCAN Clustering"+
+                              "\n 2 : OPTICS Clustering"+
+                              "\n 3 : Mean-Shift Clustering"+
                               "\n r : Restart the Application\n")
                if (userInput == "q"): break
                #elif (userInput == "0"): clustering.k_means_clustering_faiss(15, "")
                elif (userInput == "0"): clustering.k_means_clustering(13)
-               elif (userInput == "1"): leah.dbscan_clustering()
-               elif (userInput == "2"): leah.optics_clustering()
-               elif (userInput == "2"): leah.mean_shift_clustering()
+               elif (userInput == "1"): clustering.dbscan_clustering()
+               elif (userInput == "2"): clustering.optics_clustering()
+               elif (userInput == "3"): clustering.mean_shift_clustering()
             
 if __name__=="__main__":
     application()
