40de8ee9ee3745c7ee0e569718bce3804ad28c5b
glclea001 <leahmaggiegluckman@gmail.com>
2022-08-02 14:46:03 +0200

    pptk

--- pptk_test.py
+++ pptk_test.py
@@ -1,7 +1,7 @@
 import pptk 
 import numpy as np
 
-pcd = np.load("CHSEG\Data\ground_truth_OPTICS.npy")
+pcd = np.load("CHSEG\Data\ground_truth_mean-shift.npy")
 points = pcd[:,0:3]
 intensity = (pcd[:,3:4]).flatten()
 truth = (pcd[:,4:5]).flatten()
