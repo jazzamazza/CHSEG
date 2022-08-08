c3830183e1165dafd8d2dc62c8ed64067dada293
Leah Gluckman <81581707+glclea001@users.noreply.github.com>
2022-08-08 14:45:44 +0200

    Added Jemma's classification

--- PointNet++/PointNet_DataLoader.py
+++ PointNet++/PointNet_DataLoader.py
@@ -1,117 +1,85 @@
-"""
-Code adapted from https://github.com/yanx27/Pointnet_Pointnet2_pytorch.git
-"""
-
-import os
-from PointNet_DataLoader import DataLoader
-import torch
-import sys
-import importlib
 import numpy as np
-from sklearn import preprocessing 
-from PointCloudLoader import PointCloudLoader
+import open3d as o3d
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_DIR = BASE_DIR
-sys.path.append(os.path.join(ROOT_DIR, 'models'))
-
-def main_semseg():
-    '''HYPER PARAMETER'''
-    os.environ["CUDA_VISIBLE_DEVICES"] = '0' #args.gpu
-
-    NUM_CLASSES = 13
-    BATCH_SIZE = 16 
-    NUM_POINT = 4096 
-
-    #goes through both methods in pointnet data loadeer
-    DATASET = DataLoader() 
-
-    '''MODEL LOADING'''
-    model_name = 'pointnet2_sem_seg'
-    MODEL = importlib.import_module(model_name)
-    classifier = MODEL.get_model(NUM_CLASSES).cuda()
-    checkpoint = torch.load('PointNet++/best_model.pth') 
-    classifier.load_state_dict(checkpoint['model_state_dict'])
-    classifier = classifier.eval()
-
-    with torch.no_grad():
-        scene_data, scene_label = DATASET[0]
-        labels = DATASET.semantic_labels_list
-        print("labels outside", labels)
-        #now has labels appended to it
-        print("scene_data", scene_data)
-        print("length", len(scene_data[0])) #lenght is 4096
-
-        print("scene_label", scene_label)
-        x = scene_label[0]
-        print("x", x)
-        scene_label_new = np.append(scene_label, x)
-        print("scene_label_new shape", scene_label_new.shape)
-        print("scene_label_new", scene_label_new)
-        print("scene_label shape", scene_label.shape) #(799, 4096)
-        print("Outside DataLoader")
-        num_blocks = scene_data.shape[0]
-        s_batch_num = (num_blocks + BATCH_SIZE - 1) // BATCH_SIZE
-        batch_data = np.zeros((BATCH_SIZE, NUM_POINT, 9))
-        batch_label = np.zeros((BATCH_SIZE, NUM_POINT))
+class DataLoader():
+    # prepare to give prediction on each points
+    def __init__(self, block_points=4096, stride=0.5, block_size=1.0, padding=0.001):
+        self.block_points = block_points
+        self.block_size = block_size
+        self.padding = padding
+        self.stride = stride
+        self.scene_points_num = []
+        self.scene_points_list = []
+        self.semantic_labels_list = []
         
-        feat_list = [] 
-        xyz_list = []
-        for sbatch in range(s_batch_num):
-            start_idx = sbatch * BATCH_SIZE
-            end_idx = min((sbatch + 1) * BATCH_SIZE, num_blocks)
-            real_batch_size = end_idx - start_idx
-            batch_data[0:real_batch_size, ...] = scene_data[start_idx:end_idx, ...]
-            batch_label[0:real_batch_size, ...] = scene_label[start_idx:end_idx, ...]
-            torch_data = torch.Tensor(batch_data)
-            torch_data = torch_data.float().cuda()
-            torch_data = torch_data.transpose(2, 1)
-            feat, seg_pred = classifier(torch_data)
-            
-            f = feat.detach().cpu().numpy()
-            p = torch_data[:,:3,:].permute(0, 2, 1).detach().cpu().numpy()
+        path = '/content/drive/Shareddrives/CHSEG/data/church_registered_ds_pointnet.ply'
+        pcd = o3d.io.read_point_cloud(path)
+        ground_truth = np.asarray(pcd.normals)
+        print("ground_truth", ground_truth)
+        data = np.hstack((np.asarray(pcd.points), np.asarray(pcd.colors), np.asarray(pcd.normals)))
+        print("shape", data.shape)
+        print(data)
 
-            xyz_list.append(p)
-            feat_list.append(f)
-        
-        print("batch label", batch_label)
+        points = data[:, :3]
+        # ground_truth = ground_truth[:,0]
+        print(points)
+        self.scene_points_list.append(data[:, :6])
+        print("=======================")
+        print("data[:, :6]", data[:, :6])
+        print("=======================")
+        # index_ground_truth =  ground_truth[:,0]
+        index_ground_truth =  ground_truth[:,0:1]
+        self.semantic_labels_list.append(index_ground_truth)
+        coord_min, coord_max = np.amin(points, axis=0)[:3], np.amax(points, axis=0)[:3]
+        assert len(self.scene_points_list) == len(self.semantic_labels_list)
 
-        new_feat_list = np.vstack((feat_list))
-        new_xyz_list = np.vstack((xyz_list))
-        new_labels = np.vstack((scene_label_new))
+    def __getitem__(self, index):
+        point_set_ini = self.scene_points_list[index]
+        points = point_set_ini[:,:6]
+        labels = self.semantic_labels_list[index]
+        coord_min, coord_max = np.amin(points, axis=0)[:3], np.amax(points, axis=0)[:3]
+        grid_x = int(np.ceil(float(coord_max[0] - coord_min[0] - self.block_size) / self.stride) + 1)
+        grid_y = int(np.ceil(float(coord_max[1] - coord_min[1] - self.block_size) / self.stride) + 1)
+        data_room = np.array([])
+        label_room = np.array([])
+        print("grid_x:", grid_x, ", grid_y:", grid_y)
+        for index_y in range(0, grid_y):
+            print("index_y:", index_y)
+            for index_x in range(0, grid_x):
+                s_x = coord_min[0] + index_x * self.stride
+                e_x = min(s_x + self.block_size, coord_max[0])
+                s_x = e_x - self.block_size
+                s_y = coord_min[1] + index_y * self.stride
+                e_y = min(s_y + self.block_size, coord_max[1])
+                s_y = e_y - self.block_size
+                point_idxs = np.where(
+                    (points[:, 0] >= s_x - self.padding) & (points[:, 0] <= e_x + self.padding) & (points[:, 1] >= s_y - self.padding) & (
+                                points[:, 1] <= e_y + self.padding))[0]
+                if point_idxs.size == 0:
+                    continue
+                num_batch = int(np.ceil(point_idxs.size / self.block_points))
+                point_size = int(num_batch * self.block_points)
+                replace = False if (point_size - point_idxs.size <= point_idxs.size) else True
+                point_idxs_repeat = np.random.choice(point_idxs, point_size - point_idxs.size, replace=replace)
+                point_idxs = np.concatenate((point_idxs, point_idxs_repeat))
+                np.random.shuffle(point_idxs)
+                data_batch = points[point_idxs, :]
+                normlized_xyz = np.zeros((point_size, 3))   #X,Y,Z VALUES CENTERED ON THE ORIGIN
+                # normlized_xyz[:, 0] = data_batch[:, 0] / coord_max[0] #wont look like what you expecting it to
+                # normlized_xyz[:, 1] = data_batch[:, 1] / coord_max[1] #divides coordinates by maximums - squash into a square 
+                # normlized_xyz[:, 2] = data_batch[:, 2] / coord_max[2]
+                # data_batch[:, 0] = data_batch[:, 0] - (s_x + self.block_size / 2.0)
+                # data_batch[:, 1] = data_batch[:, 1] - (s_y + self.block_size / 2.0)
+                label_batch = labels[point_idxs].astype(int)
+                data_batch = np.concatenate((data_batch, normlized_xyz), axis=1)    
+                data_room = np.vstack([data_room, data_batch]) if data_room.size else data_batch  #normalized - if just take point indexes - also return orginal points - FIRST THREE COLUMNS OF DATA BATCH BEFORE NORMALIZED  
+                label_room = np.hstack([label_room, label_batch]) if label_room.size else label_batch
+        data_room = data_room.reshape((-1, self.block_points, data_room.shape[1]))
+        label_room = label_room.reshape((-1, self.block_points))
+        print("data_room shape", data_room.shape)
+        print("label_room shape", label_room.shape)
+        return data_room, label_room
+        # , data[:,6:7]
 
-        new_new_labels = np.reshape(new_labels, new_labels.shape + (1,))
-
-        print("new_labels", new_new_labels.shape) 
-        print('new_feat_list shape:', new_feat_list.shape)
-        print('new_xyz_list shape:', new_xyz_list.shape)
-
-        final_feat_list = np.vstack((new_feat_list))
-        final_xyz_list = np.vstack((new_xyz_list))
-        final_labels = np.vstack((new_new_labels))
-        
-        print('final_feat_list shape:', final_feat_list.shape)
-        print('final_xyz_list shape:', final_xyz_list.shape)
-        print('final_labels shape:', final_labels.shape)
-
-        print("final_feat_list:", final_feat_list)
-        print("final_xyz_list:", final_xyz_list)
-        print("final_labels:", final_labels)
-
-        scalar = preprocessing.MinMaxScaler()
-        normalised_feat = scalar.fit_transform(final_feat_list)
-
-        print("Calculating finalPCD")
-
-        finalPCD = np.column_stack((final_xyz_list, normalised_feat))
-        finalPCD_all = np.column_stack((final_xyz_list, final_labels, normalised_feat))
-        # np.save('/content/drive/Shareddrives/CHSEG/data/PNET_TRUTH', finalPCD)
-
-        #now we use this test_semseg_pnet_DS2.npy file for the rest
-        print("finalPCD shape:", finalPCD.shape)
-        print("*********************************")
-
-        return finalPCD, finalPCD_all
-
-if __name__ == '__main__':
-    main_semseg()
+    def __len__(self):
+        return len(self.scene_points_list)
