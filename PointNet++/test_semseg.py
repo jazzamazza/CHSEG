e90378f043d5bce5bc2af0ed85d0ff633788b422
Leah Gluckman <81581707+glclea001@users.noreply.github.com>
2022-08-08 14:47:18 +0200

    Added jemma's classification

--- PointNet++/test_semseg.py
+++ PointNet++/test_semseg.py
@@ -8,7 +8,8 @@ import torch
 import sys
 import importlib
 import numpy as np
-from sklearn import preprocessing
+from sklearn import preprocessing 
+from PointCloudLoader import PointCloudLoader
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = BASE_DIR
@@ -22,8 +23,9 @@ def main_semseg():
     BATCH_SIZE = 16 
     NUM_POINT = 4096 
 
-    DATASET = DataLoader()            
-
+    #goes through both methods in pointnet data loadeer
+    DATASET = DataLoader() 
+           
     '''MODEL LOADING'''
     model_name = 'pointnet2_sem_seg'
     MODEL = importlib.import_module(model_name)
@@ -33,12 +35,26 @@ def main_semseg():
     classifier = classifier.eval()
 
     with torch.no_grad():
-        scene_data = DATASET[0]
+        scene_data, scene_label = DATASET[0]
+        labels = DATASET.semantic_labels_list
+        print("labels outside", labels)
+        #now has labels appended to it
+        print("scene_data", scene_data)
+        print("length", len(scene_data[0])) #lenght is 4096
+
+        print("scene_label", scene_label)
+        x = scene_label[0]
+        print("x", x)
+        scene_label_new = np.append(scene_label, x)
+        print("scene_label_new shape", scene_label_new.shape)
+        print("scene_label_new", scene_label_new)
+        print("scene_label shape", scene_label.shape) #(799, 4096)
         print("Outside DataLoader")
         num_blocks = scene_data.shape[0]
         s_batch_num = (num_blocks + BATCH_SIZE - 1) // BATCH_SIZE
         batch_data = np.zeros((BATCH_SIZE, NUM_POINT, 9))
-    
+        batch_label = np.zeros((BATCH_SIZE, NUM_POINT))
+        
         feat_list = [] 
         xyz_list = []
         for sbatch in range(s_batch_num):
@@ -46,6 +62,7 @@ def main_semseg():
             end_idx = min((sbatch + 1) * BATCH_SIZE, num_blocks)
             real_batch_size = end_idx - start_idx
             batch_data[0:real_batch_size, ...] = scene_data[start_idx:end_idx, ...]
+            batch_label[0:real_batch_size, ...] = scene_label[start_idx:end_idx, ...]
             torch_data = torch.Tensor(batch_data)
             torch_data = torch_data.float().cuda()
             torch_data = torch_data.transpose(2, 1)
@@ -56,35 +73,45 @@ def main_semseg():
 
             xyz_list.append(p)
             feat_list.append(f)
+        
+        print("batch label", batch_label)
 
         new_feat_list = np.vstack((feat_list))
         new_xyz_list = np.vstack((xyz_list))
+        new_labels = np.vstack((scene_label_new))
 
+        new_new_labels = np.reshape(new_labels, new_labels.shape + (1,))
+
+        print("new_labels", new_new_labels.shape) 
         print('new_feat_list shape:', new_feat_list.shape)
         print('new_xyz_list shape:', new_xyz_list.shape)
 
         final_feat_list = np.vstack((new_feat_list))
         final_xyz_list = np.vstack((new_xyz_list))
+        final_labels = np.vstack((new_new_labels))
         
         print('final_feat_list shape:', final_feat_list.shape)
         print('final_xyz_list shape:', final_xyz_list.shape)
+        print('final_labels shape:', final_labels.shape)
 
         print("final_feat_list:", final_feat_list)
         print("final_xyz_list:", final_xyz_list)
+        print("final_labels:", final_labels)
 
         scalar = preprocessing.MinMaxScaler()
         normalised_feat = scalar.fit_transform(final_feat_list)
 
         print("Calculating finalPCD")
-        finalPCD = np.column_stack((final_xyz_list, normalised_feat))
 
-        np.save('/content/drive/MyDrive/GitHubTest/data/finalPCD.npy', finalPCD)
+        finalPCD = np.column_stack((final_xyz_list, normalised_feat))
+        finalPCD_all = np.column_stack((final_xyz_list, final_labels, normalised_feat))
+        # np.save('/content/drive/Shareddrives/CHSEG/data/PNET_TRUTH', finalPCD)
+
+        #now we use this test_semseg_pnet_DS2.npy file for the rest
         print("finalPCD shape:", finalPCD.shape)
         print("*********************************")
 
-        return finalPCD
-        #clustering = Clustering(finalPCD, "3")
-        #clustering.k_means_clustering_faiss(20, "CHSEG_finalPCD!")
+        return finalPCD, finalPCD_all
 
 if __name__ == '__main__':
     main_semseg()
