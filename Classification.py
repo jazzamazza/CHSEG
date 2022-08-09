c7b4c054b86571950a72fe1d84b4bc4c9ecc6fba
glclea001 <leahmaggiegluckman@gmail.com>
2022-08-09 16:28:47 +0200

    Made classification more efficient

--- Classification.py
+++ Classification.py
@@ -9,47 +9,40 @@ class Classification:
     def get_ground_truth(self):
           return self.true_labels, self.predicted_labels
           
-    # for raw: index = 4
-    # for cloud compare: index = 
-    # for pointnet: index =
+    # for raw: index = 4 --> x, y, z, intensity, ground_truth
+    # for cloud compare: index = 4 --> x, y, z, intensity, ground_truth, ...
+    # for pointnet: index = 3 --> x, y, z, ground_truth, ...
     def classify(self, unique_labels, y_km, t, index, file_path, file_name):
-          num_keep, num_discard = 0, 0
           self.true_labels = t[:,index:index+1] # ground truth before processing
-
           ground_truths = np.array([])
           print("t[0]", t[0])
           print("ground_truth size:", ground_truths.size)
+          
           for i in unique_labels:
-              num_keep, num_discard = 0, 0
-              for point in t[y_km == i]:
-                if (point[index] >= float(0.5)): num_discard += 1
-                else: num_keep += 1
+              # count the number of keep and discard ground truth labels in each cluster
+              num_discard = sum([1 for point in t[y_km == i] if (point[index] >= float(0.5))])
+              num_keep = len(t[y_km == i]) - num_discard
               print("num_keep:", num_keep)
               print("num_discard:", num_discard)
-              if num_keep > num_discard: 
-                ground_truths = np.append(ground_truths, 1) #changing the clusters to keep and discard
-              else: 
-                ground_truths = np.append(ground_truths, 0)
+
+              # changing the clusters to keep and discard
+              truth_val = float(1) # 0 = keep
+              if num_keep > num_discard: truth_val = float(0) # 1 = discard
+              
+              ground_truths = np.append(ground_truths, truth_val)
+
+              # set all points in cluster to majority ground truth of the cluster
+              t[y_km == i, index:index+1] = truth_val
 
           print("ground_truth:", ground_truths)
-
-          #sets cluster to majority ground truth       
-          for i in range(0, len(ground_truths)):   #i is each cluster
-            if ground_truths[i] == float(1): # if cluster == keep
-              for point in t[y_km == i]: # set ground truth of each point to keep
-                # print("point", point)
-                t[y_km == i, index:index+1] = float(1)
-            else:
-              for point in t[y_km == i]:
-                t[y_km == i, index:index+1] = float(0)
+          print("unique_labels:", unique_labels)
+          assert len(ground_truths) == len(unique_labels)
                 
           print("t shape", np.shape(t))
-          print("truth", t[0])
+          print("t[0]", t[0])
           print("t", t)
 
           np.save(file_path + file_name + ".npy", t)
-
-          
           self.predicted_labels = t[:,index:index+1] # predicted_ground_truths
     
     # def visualise_classification(self, pcd):
