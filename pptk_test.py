8a1f6c87c03fcc03b842350236c64a3541c8777c
glclea001 <leahmaggiegluckman@gmail.com>
2022-08-12 13:23:00 +0200

    changes

--- pptk_test.py
+++ pptk_test.py
@@ -1,10 +1,16 @@
 import pptk 
 import numpy as np
 
-pcd = np.load("Visualise_Output\\raw_meanshift.npy")
+pcd = np.load("Visualise_Output\\raw_DBSCAN.npy")
 points = pcd[:,0:3]
 intensity = (pcd[:,3:4]).flatten()
 truth = (pcd[:,4:5]).flatten()
+
+
+l = len(pcd)
+for i in range(l):
+    if pcd[i,4:5][0] == float(1):
+      print(pcd[i,4:5])
 
 view = pptk.viewer(points, intensity, truth)
 
