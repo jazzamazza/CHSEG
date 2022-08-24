130ef563b48ec3864c18fba246cbd8c3c7a94b28
glclea001 <leahmaggiegluckman@gmail.com>
2022-08-24 09:00:19 +0200

    Pointnet ground truth working

--- PointNet++/PointNet_DataLoader.py
+++ PointNet++/PointNet_DataLoader.py
@@ -10,36 +10,33 @@ class DataLoader():
         self.stride = 0.5
         self.scene_points_num, self.scene_points_list, self.semantic_labels_list  = [], [], []
 
-        path = 'Data/church_registered_downsampled_0.075.ply'
+        path = '/content/drive/Shareddrives/CHSEG/data/church_registered_ds_pointnet0.075.ply'
         pcd = o3d.io.read_point_cloud(path)
         ground_truth = np.ceil(np.asarray(pcd.normals)[:,0:1])
         print("ground_truth", ground_truth)
-        data = np.hstack((np.asarray(pcd.points), np.asarray(pcd.colors)))
-        print("shape", data.shape)
-        print(data)
-
+        data = np.hstack((np.asarray(pcd.points), np.asarray(pcd.colors))) #xyz intensityx3
         points = data[:, :3]
         print(points)
         self.scene_points_list.append(data[:, :6])
         print("=======================")
         print("data[:, :6]", data[:, :6])
         print("=======================")
-        # index_ground_truth =  ground_truth[:,0:1]
         self.semantic_labels_list.append(ground_truth)
         coord_min, coord_max = np.amin(points, axis=0)[:3], np.amax(points, axis=0)[:3]
         assert len(self.scene_points_list) == len(self.semantic_labels_list)
 
     def __getitem__(self, index):
         point_set_ini = self.scene_points_list[index]
-        points = point_set_ini[:,:6]
+        points = point_set_ini[:,:6]  
         labels = self.semantic_labels_list[index]
-        print("********labels:", labels)
         coord_min, coord_max = np.amin(points, axis=0)[:3], np.amax(points, axis=0)[:3]
         grid_x = int(np.ceil(float(coord_max[0] - coord_min[0] - self.block_size) / self.stride) + 1)
         grid_y = int(np.ceil(float(coord_max[1] - coord_min[1] - self.block_size) / self.stride) + 1)
         data_room, label_room = np.array([]), np.array([])
+        
         for index_y in range(0, grid_y):
             print("index_y:", index_y)
+            
             for index_x in range(0, grid_x):
                 s_x = coord_min[0] + index_x * self.stride
                 e_x = min(s_x + self.block_size, coord_max[0])
@@ -61,13 +58,13 @@ class DataLoader():
                 data_batch = points[point_idxs, :]
                 normlized_xyz = np.zeros((point_size, 3))   #X,Y,Z VALUES CENTERED ON THE ORIGIN
                 label_batch = labels[point_idxs].astype(int)
-                data_batch = np.concatenate((data_batch, normlized_xyz), axis=1)    
+                data_batch = np.concatenate((data_batch, normlized_xyz), axis=1)                
                 data_room = np.vstack([data_room, data_batch]) if data_room.size else data_batch  #normalized - if just take point indexes - also return orginal points - FIRST THREE COLUMNS OF DATA BATCH BEFORE NORMALIZED  
-                label_room = np.hstack([label_room, label_batch]) if label_room.size else label_batch
+                label_room = np.vstack([label_room, label_batch]) if label_room.size else label_batch
+                
         data_room = data_room.reshape((-1, self.block_points, data_room.shape[1]))
         label_room = label_room.reshape((-1, self.block_points))
-        print("data_room shape", data_room.shape)
-        print("label_room shape", label_room.shape)
+
         return data_room, label_room
 
     def __len__(self):
