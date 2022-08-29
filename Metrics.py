eca42ab6e8adf25ef7203ad214e52d220430e2e7
Jared May <jared@jaredmay.co.za>
2022-08-29 14:21:12 +0200

    metrics updates

--- Metrics.py
+++ Metrics.py
@@ -13,6 +13,9 @@ from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import PrecisionRecallDisplay
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import silhouette_score
+from sklearn.metrics import davies_bouldin_score
+from sklearn.metrics import rand_score
 
 import matplotlib.cm as cm
 
@@ -23,122 +26,6 @@ class Evaluation:
         self.y_pred = truth_labels[:,1:2]
         self.class_metrics = ClassificationMetrics(self.y_pred, self.y_true)
 
-    # def silhouette_kmeans(self):
-
-    #     x = self.pcd
-
-    #     K = range(2, 20)
-    #     for k in K:
-            
-    #         clusterer = KMeans(n_clusters=k)  # for k-means and k-medoids
-
-    #         cluster_labels = clusterer.fit_predict(x)
-
-    #         silhouette_avg = silhouette_score(x, cluster_labels)
-    #         print(
-    #             "For n_clusters =",
-    #             k,
-    #             "The average silhouette_score is :",
-    #             silhouette_avg,
-    #         )
-    #         sample_silhouette_values = silhouette_samples(x, cluster_labels)
-
-    #         y_lower = 10
-    #         for i in range(k):
-    #             # Aggregate the silhouette scores for samples belonging to
-    #             # cluster i, and sort them
-    #             ith_cluster_silhouette_values = sample_silhouette_values[
-    #                 cluster_labels == i
-    #             ]
-
-    #             ith_cluster_silhouette_values.sort()
-
-    #             size_cluster_i = ith_cluster_silhouette_values.shape[0]
-    #             y_upper = y_lower + size_cluster_i
-
-    #             color = cm.nipy_spectral(float(i) / k)
-    #             ax1.fill_betweenx(
-    #                 np.arange(y_lower, y_upper),
-    #                 0,
-    #                 ith_cluster_silhouette_values,
-    #                 facecolor=color,
-    #                 edgecolor=color,
-    #                 alpha=0.7,
-    #             )
-
-    #             ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
-
-    #             # Compute the new y_lower for next plot
-    #             y_lower = y_upper + 10  # 10 for the 0 samples
-
-    #         ax1.set_title("The silhouette plot for the various clusters.")
-    #         ax1.set_xlabel("The silhouette coefficient values")
-    #         ax1.set_ylabel("Cluster label")
-
-    #         ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
-
-    #         ax1.set_yticks([])  # Clear the yaxis labels / ticks
-    #         ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
-
-    #         # 2nd Plot showing the actual clusters formed
-    #         colors = cm.nipy_spectral(cluster_labels.astype(float) / k)
-    #         ax2.scatter(
-    #             x[:, 0],
-    #             x[:, 1],
-    #             marker=".",
-    #             s=30,
-    #             lw=0,
-    #             alpha=0.7,
-    #             c=colors,
-    #             edgecolor="k",
-    #         )
-
-    #         # Labeling the clusters
-    #         centers = clusterer.cluster_centers_
-    #         # Draw white circles at cluster centers
-    #         ax2.scatter(
-    #             centers[:, 0],
-    #             centers[:, 1],
-    #             marker="o",
-    #             c="white",
-    #             alpha=1,
-    #             s=200,
-    #             edgecolor="k",
-    #         )
-
-    #         for i, c in enumerate(centers):
-    #             ax2.scatter(c[0], c[1], marker="$%d$" % i, alpha=1, s=50, edgecolor="k")
-
-    #         ax2.set_title("The visualization of the clustered data.")
-    #         ax2.set_xlabel("Feature space for the 1st feature")
-    #         ax2.set_ylabel("Feature space for the 2nd feature")
-
-    #         plt.suptitle(
-    #             "Silhouette analysis for KMeans clustering on sample data with n_clusters = %d"
-    #             % k,
-    #             fontsize=14,
-    #             fontweight="bold",
-    #         )
-
-    #     plt.show()
-
-    # def db_index(self):
-    #     x = self.pcd
-
-    #     results = {}
-
-    #     for i in range(2, 100):
-    #         kmeans = KMeans(n_clusters=i, random_state=30)
-    #         labels = kmeans.fit_predict(x)
-    #         db_index = davies_bouldin_score(x, labels)
-    #         results.update({i: db_index})
-    #         print({i: db_index})
-
-    #     plt.plot(list(results.keys()), list(results.values()))
-    #     plt.xlabel("Number of clusters")
-    #     plt.ylabel("Davies-Boulding Index")
-    #     plt.show()
-    
     def check_truth(self):
         print("checking self.y_predict:")
         for i in self.y_pred:
@@ -244,5 +131,16 @@ class ClassificationMetrics:
         #           "\nMean Squared Error:", str(sq).replace('.', ','))
 
 class ClusterMetrics:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, y_true, y_pred, cluster_labels, input_pcd) -> None:
+        self.y_true = y_true
+        self.y_pred = y_pred
+        self.cluster_labels = cluster_labels
+        self.input_pcd = input_pcd
+        
+    def run_metric(self, metric_choice):
+        if metric_choice == "sill":
+            return silhouette_score(self.input_pcd, self.cluster_labels)
+        if metric_choice == "db":
+            return davies_bouldin_score(self.input_pcd, self.cluster_labels)
+        if metric_choice == "rand":
+            return rand_score(self.y_true, self.cluster_labels)
