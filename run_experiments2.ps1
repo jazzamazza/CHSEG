805bebe5028b66e57832511c212b87f9bb99fdc6
Jared May <jared@jaredmay.co.za>
2022-09-09 03:42:54 +0200

    updates

--- run_experiments2.ps1
+++ run_experiments2.ps1
@@ -1,7 +1,13 @@
 python.exe RunExperiment.py kmeans raw 0.050 100 750
+rm .\Data\Clustered\kmeans\*.npy
 python.exe RunExperiment.py kmeans geo 0.050 100 750
+rm .\Data\Clustered\kmeans\*.npy
 python.exe RunExperiment.py kmeans pnet 0.050 100 750
+rm .\Data\Clustered\kmeans\*.npy
 
 python.exe RunExperiment.py birch raw 0.075 100 750
+rm .\Data\Clustered\birch\*.npy
 python.exe RunExperiment.py birch geo 0.075 100 750
+rm .\Data\Clustered\birch\*.npy
 python.exe RunExperiment.py birch pnet 0.075 100 750
+rm .\Data\Clustered\birch\*.npy
--- run_experiments3.ps1
+++ run_experiments3.ps1
@@ -1,3 +1,6 @@
 python.exe RunExperiment.py cure geo 0.075 100 750
+rm .\Data\Clustered\cure\*.npy
 python.exe RunExperiment.py cure raw 0.075 100 750
+rm .\Data\Clustered\cure\*.npy
 python.exe RunExperiment.py cure pnet 0.075 100 750
+rm .\Data\Clustered\cure\*.npy
