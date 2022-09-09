be510a01f783bdaa5fb2f41fde5903547e6f7521
glclea001 <leahmaggiegluckman@gmail.com>
2022-09-09 12:44:02 +0200

    Added documentation

--- Outputting.py
+++ Outputting.py
@@ -1,15 +1,12 @@
-# OUTPUT
+'''Reponsible for Outputting Results'''
 
-# results_file = "MY_THESIS_RESULTS\mtr_Raw\Results_Raw_Final_Thurs_14_10.txt"
-results_file = "Results_PointNet_Sat.txt"
-# results_file = "Results_Raw_Final_Thurs_14_10_testing_db.txt"
-
-# results_file = "Output_Data\Results_PNet_testing_k.txt"
-# results_file = "Output_Data\Results_CldCmp_wed_8_35.txt"
-# results_file = "Output_Data\Results_PNet.txt"
-
+results_file = "Results_PointNet_Testing.txt"
 img_path = "Output\\"
+
 def write_results_to_file(results):
+    '''Method to write results to file
+    args:
+        results: an array of strings to write to file'''
     print("...writing")
     file = open(results_file, 'a')
     file.write(results + "\n")
