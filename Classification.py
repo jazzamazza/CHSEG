376957a52385900257e0dfcbd35eea363d7508ce
glclea001 <leahmaggiegluckman@gmail.com>
2022-09-09 12:41:15 +0200

    Added documentation

--- Classification.py
+++ Classification.py
@@ -1,24 +1,38 @@
 import numpy as np
-from Outputting import write_results_to_file
 import pptk
 
 class Classification:
+    '''This class is reponsible for classifying a point cloud into 'keep' (0) and 'discard' (1) clusters'''
     def __init__(self):
+      '''Initialise class parameters to None'''
       self.true_labels = None
       self.predicted_labels = None
 
     def get_ground_truth(self):
+      '''Method to get the attribute for the true ground truth labels and the predicted ground truth labels
+      returns:
+        self.true_labels: an array containing the true ground truth labels of the point cloud
+        self.predicted_labels: an array containing the predicted ground truth labels of the point cloud
+      '''
       print("\n******************Get Ground Truth*******************")
       self.get_attributes(self.true_labels, "self.true_labels")
       self.get_attributes(self.predicted_labels, "self.predicted_labels")
       return self.true_labels, self.predicted_labels
           
     def classify(self, unique_labels, y_km, t, index, file_path, file_name):
+      '''Method to label each produced cluster as the majority ground truth label in each cluster
+      args:
+        unique_labels: the cluster indexes
+        y_km: the produced clusters
+        t: the point cloud containing the actual ground truth at index
+        index: the index of the actual ground truth in the array t
+        '''
       print("\n******************Classification*******************")
-      
       ground_truths = np.array([])
       self.true_labels = np.array(t[:, index:index+1])
       total_keep, total_discard = 0, 0
+
+      # iterate through each cluster
       for i in unique_labels:
             # count the number of keep and discard ground truth labels in each cluster
             num_discard = sum([1 for point in t[y_km == i] if (point[index] > float(0.5))])
@@ -39,23 +53,23 @@ class Classification:
       print("ground_truth:", ground_truths, "\nunique_labels:", unique_labels)
       print("total_discard:", total_discard, "total_keep:", total_keep)
       
-      # class_file_name = file_path + file_name + ".npy"
-      # np.save(class_file_name, t)
-      # write_results_to_file("-------Classified PCD filename: " + class_file_name + "-------")
       self.predicted_labels = t[:,index:index+1] # predicted_ground_truths
       self.visualise_classification(t)
     
     def get_attributes(self, arr, title):
+      '''Print attributes of a given array
+      args: 
+        arr: the array to print attributes of
+        title: the title of the array to display'''
       print("================", title, "\n:", arr)
-      print("Length:", len(arr))
-      print("Shape:", np.shape(arr))
+      print("Length:", len(arr), "\nShape:", np.shape(arr))
 
     def visualise_classification(self, t):
+      '''Visualise the classified point cloud in pptk
+      args: 
+        t: the point cloud to display'''
       xyz = t[:,0:3]
       predicted_ground_truth = self.predicted_labels.flatten()
       true_ground_truth = self.true_labels.flatten()
-      print("predicted_labels:", predicted_ground_truth)
-      print("true_labels:", true_ground_truth)
-      # intensity1d = (t[:,3:4]).flatten()
-      # pptk.viewer(xyz, intensity1d, predicted_ground_truth, true_ground_truth)
+      print("predicted_labels:", predicted_ground_truth, "\ntrue_labels:", true_ground_truth)
       pptk.viewer(xyz, predicted_ground_truth, true_ground_truth)
--- Clustering.py
+++ Clustering.py
@@ -1,28 +1,36 @@
 import numpy as np
-from sklearn.cluster import KMeans, MeanShift, estimate_bandwidth, DBSCAN, OPTICS
+from sklearn.cluster import KMeans, MeanShift, DBSCAN, OPTICS
 from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics import *
 from kneed import KneeLocator
 from Outputting import write_results_to_file
-from Clust_Vis import *
 import pptk
 
 # Clustering class with various clustering methods
 class Clustering:
+     '''This class is responsible for partitioning a point cloud into clusters'''
      def __init__(self, pointCloud, pcd_choice):
+          '''Initialise class variables
+          args:
+               pointCloud: the point cloud to cluster
+               pcd_choice: the dataset choice (raw, cldCmp or pnet++)'''
           self.pcd = pointCloud
           if (pcd_choice == "1"): self.type = "raw"
           elif (pcd_choice == "2"): self.type = "cldCmp"
           elif (pcd_choice == "3"): self.type = "pnet++"
-
-          self.vis = Clust_Vis()
-     
-     # k means clustering method --> clusters a dataset into k (given) clusters
-     def k_means_clustering(self, k):
-          x = self.pcd
-          k = 2000
+          
+     def k_means_clustering(self, k, n_init=10):
+          '''Cluster point cloud using k-means clustering method
+          arg: 
+               k: the number of clusters to partition the point cloud into
+               n_init: number of iterations of k-means (default = 10)
+          returns:
+               unique_labels: an array containg the cluster indexes
+               y_km: the produced clusters
+               A suffix corresponding to the name of the clustering method
+          '''
           print("\n------------------k means---------------------")
-          n_init = 10
+          x = self.pcd
           kmeans = KMeans(n_clusters=k, n_init=n_init).fit(x) # number of clusters (k)
           y_km = kmeans.predict(x)
 
@@ -32,48 +40,48 @@ class Clustering:
           unique_labels = np.unique(y_km)
           self.get_information(y_km, x, unique_labels)
 
-          
-          # self.vis.vis_k_means(unique_labels, kmeans.cluster_centers_, y_km, x)
-
           self.visualise_clustering(y_km, x)
           return unique_labels, y_km, "_kmeans"
 
-      # OPTIC
-     def optics_clustering(self):
+     def optics_clustering(self, min_samp=10, xi=0.05, min_cluster_sz=25, max_e=100):
+          '''Cluster point cloud using OPTICS clustering method
+          arg: 
+               min_samp:
+               xi:
+               min_cluster_size: (default=25)
+               max_e:
+          returns:
+               unique_labels: an array containg the cluster indexes
+               y_op: the produced clusters
+               A suffix corresponding to the name of the clustering method
+          '''
           print("***************OPTICS CLUSTERING***************")
           X = self.pcd
-          min_samp = 80
-          xi = 0.02 #0.05
-          min_cluster_sz = 20
-          max_e = 1000
-          print("starting optics method")
-          clust = OPTICS(min_samples=min_samp, xi=xi, min_cluster_size=min_cluster_sz, max_eps=max_e).fit(X)
-          
+          clust = OPTICS(min_samples=min_samp, xi=xi, min_cluster_size=min_cluster_sz, max_eps=max_e).fit(X)  
           y_op = clust.fit_predict(X)
-          print("finished optics method")
 
           arrResults = ["*************OPTICS Parameters*************", "min_samples: " + str(min_samp), "min_cluster_size:" + str(min_cluster_sz), "xi:" + str(xi), "max_eps:" + str(max_e)]
           self.write_results(arrResults)
           
           unique_labels = np.unique(y_op)
           self.get_information(y_op, X, unique_labels)
-          # self.vis.vis_OPTICS(X, clust.reachability_[clust.ordering_], y_op)
           
           self.visualise_clustering(y_op, X)
           return unique_labels, y_op, "_OPTICS"
 
-     # DBSCAN 
-     def dbscan_clustering(self):
+     def dbscan_clustering(self, min_samples_=6):
+          '''Cluster point cloud using DBSCAN clustering method
+          arg: 
+               k: the number of clusters to partition the point cloud into
+               min_samples: the minimum number of points per cluster
+          returns:
+               unique_labels: an array containg the cluster indexes
+               y_db: the produced clusters
+               A suffix corresponding to the name of the clustering method
+          '''
           print("***************DBSCAN CLUSTERING***************")
-          print("starting dbscan_clustering")
           X = self.pcd
-
-          # self.calculate_best_db_parameters(X)
-
-          min_samples_ = 50 #for cloud compare with 25 features
-          # min_samples_ = 7 # for raw point cloud with 4 features
           e = self.calculateElbow(min_samples_)
-          print("e:", e)
 
           arrResults = ["*************DBSCAN Parameters*************", "min_samples:"+str(min_samples_), "e:"+str(e).replace('.', ',')]
           self.write_results(arrResults)
@@ -86,55 +94,65 @@ class Clustering:
           
           unique_labels = np.unique(y_db)
           self.get_information(y_db, X, unique_labels)
-          # self.vis(X, y_db, db, core_samples_mask)
-
           self.visualise_clustering(y_db, X)
 
           return unique_labels, y_db, "_DBSCAN"
 
      def calculateElbow(self, n):
-           # FIND OPTIMAL EPSILON VALUE: use elbow point detection method 
+          '''Calculate the value of e for DBSCAN
+          args: 
+               n: min_samples
+          returns:
+               eps: the value of e for DBSCAN
+          '''
+          # Find optimal epsilon value: use elbow point detection method 
           nearest_neighbors = NearestNeighbors(n_neighbors=n)
           neighbors = nearest_neighbors.fit(self.pcd)
           distances, _ = neighbors.kneighbors(self.pcd)
           distances = np.sort(distances[:,4], axis=0)
           
-          # IDENTIFY ELBOW POINT:
-          print("Identify Elbow Point:")
+          # Identify Elbow Point
           i = np.arange(len(distances))
           knee = KneeLocator(i, distances, S=1, curve='convex', direction='increasing', interp_method='polynomial')
           knee.plot_knee()
           eps = distances[knee.knee]
-          # self.vis.vis_elbow_method(distances)
           return eps
 
-     # MeanShift
-     def mean_shift_clustering(self):
+     def mean_shift_clustering(self, bandwidth=1):
+          '''Cluster point cloud using mean-shift clustering method
+          arg: 
+               bandwidth: (default = 1)
+          returns:
+               unique_labels: an array containg the cluster indexes
+               y_ms: the produced clusters
+               A suffix corresponding to the name of the clustering method
+          '''
           print("***************MEAN-SHIFT CLUSTERING***************")
           X = self.pcd
-          quantile = 0 #0.2 #0.2
-          n_samples = 0 #100 #500
-          # bandwidth = estimate_bandwidth(X, quantile=quantile, n_samples=n_samples)
-
-          bandwidth = 1 #0.09
-          print("bandwidth:", bandwidth)
           ms = MeanShift(bandwidth=bandwidth, bin_seeding=True).fit(X)
           y_ms = ms.predict(X)
 
-          self.write_results(["*************MEAN-SHIFT Parameters*************", "quantile:" + str(quantile), "n_samples:" + str(n_samples), "bandwidth:" + str(bandwidth).replace('.', ','),  "bin_seeding: True"])
+          self.write_results(["*************MEAN-SHIFT Parameters*************", "bandwidth:" + str(bandwidth).replace('.', ','),  "bin_seeding: True"])
           unique_labels = np.unique(y_ms)
           self.get_information(y_ms, X, unique_labels)
-          # self.vis.vis_mean_shift(X, y_ms, ms)
-
           self.visualise_clustering(y_ms, X)
 
           return unique_labels, y_ms, "_meanshift"
 
      def write_results(self, arrResults):
+          '''Write results to a file
+          args: 
+               arrResults: the array of strings to write to file'''
           for r in arrResults:
                write_results_to_file(r)
 
      def get_information(self, labels, X, unique_labels):
+          '''Method to get information about produced clusters
+          args:
+               labels: the cluster label of each cluster
+               X: the point Cloud
+               unique_labels: an array of unique cluster labels
+          '''
           print("Unique Labels:", unique_labels)
           no_clusters = len(np.unique(labels))
           no_noise = np.sum(np.array(labels) == -1, axis=0)
@@ -143,7 +161,7 @@ class Clustering:
           print(clust)
           print(noise)
 
-          sil_score = 0 #silhouette_score(X, labels)
+          sil_score = silhouette_score(X, labels)
           db_index = davies_bouldin_score(X, labels)
           print("Silhouette Coefficient: %0.3f" % sil_score)
           print("Davies Bouldin Score: %0.3f" % db_index)
@@ -152,16 +170,8 @@ class Clustering:
           self.write_results(arrResults)
      
      def visualise_clustering(self, labels, x):
-          xyz = x[:,0:3]
-          pptk.viewer(xyz, labels.flatten())
-
-     def calculate_best_db_parameters(self, X):
-          self.write_results(["TESTING SILHOUETTE SCORE FOR DIFFERENT DB MIN_SAMPLES VALUES:"])
-          for i in range(8, 100):
-               print("min_samples:", i)
-               e = self.calculateElbow(i)
-               db = DBSCAN(eps=e, min_samples=i).fit(X)
-               y_db = db.fit_predict(X)
-               sil_score = silhouette_score(X, y_db)
-               print("Silhouette Coefficient: %0.3f" % sil_score)
-               self.write_results(["min_samples: " + str(i), "silhouette coefficient: ", str(sil_score)])
+          '''Visualise Clustered Point Cloud with pptk
+          args:
+               labels: the label associated with each cluster
+               x: the point cloud to visualise'''
+          pptk.viewer(x[:,0:3], labels.flatten())
