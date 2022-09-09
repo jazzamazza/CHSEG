b3d6967988ae1228165bb8588560ee9f8563df30
Jared May <jared@jaredmay.co.za>
2022-09-09 16:35:11 +0200

    results

--- run_experiments2.ps1
+++ run_experiments2.ps1
@@ -1,8 +1,8 @@
-python.exe RunExperiment.py kmeans raw 0.050 276 750
+# python.exe RunExperiment.py kmeans raw 0.050 276 750
+# rm .\Data\Clustered\kmeans\*.npy
+python.exe RunExperiment.py kmeans pnet 0.050 208 750
 rm .\Data\Clustered\kmeans\*.npy
-python.exe RunExperiment.py kmeans geo 0.050 100 750
-rm .\Data\Clustered\kmeans\*.npy
-python.exe RunExperiment.py kmeans pnet 0.050 100 750
+python.exe RunExperiment.py kmeans geo 0.075 100 750
 rm .\Data\Clustered\kmeans\*.npy
 
 python.exe RunExperiment.py birch raw 0.075 100 750
