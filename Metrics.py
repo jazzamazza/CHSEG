9f9676a682fe73cf74165499e0c765c38fdb919a
jemmasundelson <66670261+jemmasundelson@users.noreply.github.com>
2022-10-17 12:15:05 +0200

    Update Metrics.py

--- Metrics.py
+++ Metrics.py
@@ -1,50 +1,36 @@
-from curses import raw
-from operator import truth
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.cluster import KMeans, dbscan
-import open3d as o3d
-from mpl_toolkits import mplot3d
-from datetime import datetime
-from yaml import load
-import laspy as lp
-from scipy.spatial.distance import cdist
 from sklearn.metrics import silhouette_samples, silhouette_score
 import matplotlib.cm as cm
-from sklearn.cluster import DBSCAN
-import faiss
-from sklearn_extra.cluster import KMedoids #pip install https://github.com/scikit-learn-contrib/scikit-learn-extra/archive/master.zip
-#from sklearn_extra.cluster import KMedians
-import sklearn_extensions as ske
-from sklearn.metrics.pairwise import (
-    pairwise_distances,
-    pairwise_distances_argmin,
-)
 from sklearn.mixture import GaussianMixture
-from pyclustering.cluster.clarans import clarans;
-from pyclustering.utils import timedcall;
-from pyclustering.cluster import cluster_visualizer_multidim
 from pyclustering.cluster.kmedians import kmedians
-from pyclustering.cluster import cluster_visualizer
-from pyclustering.cluster.kmeans import kmeans, kmeans_visualizer
-from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
-from sklearn.cluster import AffinityPropagation
-from sklearn import metrics
-from pyclustering.cluster.silhouette import silhouette
 from sklearn.metrics import davies_bouldin_score
+from fcmeans import FCM
+from pyclustering.cluster.silhouette import silhouette_ksearch_type, silhouette_ksearch
+from pyclustering.cluster.center_initializer import random_center_initializer
+from pyclustering.cluster.encoder import type_encoding, cluster_encoder
+from sklearn.metrics import *
+import itertools
+from scipy import linalg
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from sklearn import mixture
+from sklearn.cluster import KMeans
 
+# testing claass to assess the perfromance of the metrics 
 class Testing:
-     def __init__(self, pointCloud, pcd_choice):
+     def __init__(self, pointCloud):
           self.pcd = pointCloud
-        #   if (pcd_choice == "1"): self.type = "raw"
-        #   elif (pcd_choice == "2"): self.type = "cldCmp"
-        #   elif (pcd_choice == "3"): self.type = "pnet++"
 
      def silhouette_kmeans(self):
-    
+          
+          # x = point cloud 
           x = self.pcd
-     
-          K = range(2, 20)
+
+          print("--------- silhouette k-means ----------")
+
+         # range is 2 to 100 clusters 
+          K = range(2, 101)
           for k in K:
                fig, (ax1, ax2) = plt.subplots(1, 2)
                fig.set_size_inches(18, 7)
@@ -59,10 +45,11 @@ class Testing:
 
                # Initialize the clusterer with n_clusters value and a random generator
                # seed of 10 for reproducibility.
-               clusterer = KMeans(n_clusters= k)      #for k-means and k-medoids
+               clusterer = KMeans(n_clusters= k, n_init=10)      #for k-means and k-medoids
                
                cluster_labels = clusterer.fit_predict(x)
 
+               # get silhouette score for k clusters 
                silhouette_avg = silhouette_score(x, cluster_labels)
                print(
                     "For n_clusters =",
@@ -71,7 +58,8 @@ class Testing:
                          silhouette_avg,
                )
                sample_silhouette_values = silhouette_samples(x, cluster_labels)
-     
+
+               # the rest of the code plots the visual for the silhouette score 
                y_lower = 10
                for i in range(k):
                     # Aggregate the silhouette scores for samples belonging to
@@ -142,20 +130,269 @@ class Testing:
 
           plt.show()
 
-     def db_index(self):
+     # silhouette score for gaussian mixture model 
+     def silhouette_GMM(self):
+       
           x = self.pcd
 
+          print("--------- silhouette GMM ----------")
+
+        # choosing range of k between 2 clusters and 100 clusters 
+          K = range(2,101)
+          for k in K:
+              
+               # Initialize the clusterer with n_clusters value and a random generato
+               clusterer = GaussianMixture(n_components=k, covariance_type='full', random_state=0)
+               cluster_labels = clusterer.fit_predict(x)
+
+                # obtain silhouette score 
+               silhouette_avg = silhouette_score(x, cluster_labels, metric='euclidean')
+               print("{",k,": ", silhouette_avg,"}")
+
+     # silhouette score for fuzzy c-means         
+     def silhouette_fuzzy_cmeans(self):
+       # x = original point cloud 
+        x = self.pcd
+
+        print("--------- silhouette fuzzy c-means ----------")
+
+        # choosing range of k between 2 clusters and 100 clusters 
+        K = range(2, 100)
+        for k in K:
+            
+            # initialise FCM, fit the data and get the labels
+            fcm = FCM(n_clusters=k)
+            fcm.fit(x)
+            fcm_labels = fcm.predict(x)
+
+            # obtain the silhouette score 
+            sil = silhouette_score(x, fcm_labels)
+            print("{",k,":", sil,"}")
+
+     # silhou
+     def silhouette_kmedians(self):
+       # x = original point cloud 
+       x = self.pcd
+       print("--------- silhouette k-medians ----------")
+
+        # obtain silhouette scores, from 2 to 100 clusters 
+       search_instance = silhouette_ksearch(x, 2, 101, algorithm=silhouette_ksearch_type.KMEDIANS).process()
+
+       amount = search_instance.get_amount()
+       scores = search_instance.get_scores()
+       print("amount", amount)
+       # get silhouette scores 
+       print("Scores: '%s'" % str(scores))
+
+
+     def db_index_kmeans(self):
+           # x = original point cloud 
+          x = self.pcd
+
+          print("--------- db index k-means ----------")
           results = {}
           
-          for i in range(2,100):
-               kmeans = KMeans(n_clusters=i, random_state=30)
-               labels = kmeans.fit_predict(x)
+          # range is from 2 to 100 clusters 
+          for i in range(2, 101):
+
+               # initialise k-means
+               km = KMeans(n_clusters=i, random_state=0)
+               # get labels 
+               labels = km.fit_predict(x)
+               # obtain db index 
                db_index = davies_bouldin_score(x, labels)
                results.update({i: db_index})
                print({i: db_index})
 
-          plt.plot(list(results.keys()), list(results.values()))
-          plt.xlabel("Number of clusters")
-          plt.ylabel("Davies-Boulding Index")
+
+     def db_index_GMM(self):
+          # x = original point cloud 
+          x = self.pcd
+
+          print("--------- db index GMM ----------")
+          results = {}
+          
+          # range is 2 clusters to 100 clusters 
+          for i in range(2,101):
+              # intialise GMM
+               gm = GaussianMixture(n_components=i, covariance_type='full', random_state=0)
+              # obtain labels 
+               labels = gm.fit_predict(x)
+              # get db index 
+               db_index = davies_bouldin_score(x, labels)
+               results.update({i: db_index})
+               print({i: db_index})
+
+    
+     def db_index_fuzzy_cmeans(self):
+          # x = original point cloud 
+          x = self.pcd
+
+          print("--------- db index fuzzy c-means ----------")
+          results = {}
+          
+          # range is 2 clusters to 100 clusters 
+          for i in range(2,101):
+              # initialise fFCM 
+               fcm = FCM(n_clusters=i)
+               fcm.fit(x)
+               labels = fcm.predict(x)
+               #obtain db index 
+               db_index = davies_bouldin_score(x, labels)
+               results.update({i: db_index})
+               print({i: db_index})
+     
+     def db_index_Kmedians(self):
+          # x = original point cloud 
+          x = self.pcd
+
+          print("--------- db index K-medians ----------")
+          results = {}
+        
+        # range is 2 clusters to 100 clusters 
+          for i in range(2,101):
+              # intialise k-medians 
+              initial_medians =  random_center_initializer(x, i).initialize()
+              kmedians_instance = kmedians(x, initial_medians)
+              clusters = kmedians_instance.get_clusters()
+
+
+            # get labels 
+              type_repr = kmedians_instance.get_cluster_encoding()
+              type_encoder = cluster_encoder(type_repr, clusters, x)
+              type_encoder.set_encoding(type_encoding.CLUSTER_INDEX_LABELING)
+              labels_km = type_encoder.get_clusters()
+
+            # obtain db index 
+              db_index = davies_bouldin_score(x, labels_km)
+              results.update({i: db_index})
+              print({i: db_index})
+
+    
+     # get BIC score for GMM - adapted from Scikit learn 
+     def BIC(self):
+          # x = original point cloud 
+          x = self.pcd
+          lowest_bic = np.infty
+          bic = []
+          n_components_range = range(1, 70)
+          cv_types = ["spherical", "tied", "diag", "full"] #initialise the model with the four different covariance types 
+          for cv_type in cv_types:
+               for n_components in n_components_range:
+               # Fit a Gaussian mixture with EM
+                    gmm = mixture.GaussianMixture(
+                         n_components=n_components, covariance_type=cv_type, max_iter=600
+                    )
+                    gmm.fit(x)
+                    bic.append(gmm.bic(x))
+                    if bic[-1] < lowest_bic:
+                         lowest_bic = bic[-1]
+                         best_gmm = gmm
+
+          bic = np.array(bic)
+          color_iter = itertools.cycle(["navy", "turquoise", "cornflowerblue", "darkorange"])
+          clf = best_gmm
+          bars = []
+
+          # Plot the BIC scores
+          plt.figure(figsize=(8, 6))
+          spl = plt.subplot(2, 1, 1)
+          for i, (cv_type, color) in enumerate(zip(cv_types, color_iter)):
+               xpos = np.array(n_components_range) + 0.2 * (i - 2)
+               bars.append(
+                    plt.bar(
+                     xpos,
+                     bic[i * len(n_components_range) : (i + 1) * len(n_components_range)],
+                     width=0.2,
+                     color=color,
+               )
+          )
+
+          plt.xticks(n_components_range)
+          plt.ylim([bic.min() * 1.01 - 0.01 * bic.max(), bic.max()])
+          plt.title("BIC score per model")
+          xpos = (
+               np.mod(bic.argmin(), len(n_components_range))
+               + 0.65
+               + 0.2 * np.floor(bic.argmin() / len(n_components_range))
+          )
+
+          plt.text(xpos, bic.min() * 0.97 + 0.03 * bic.max(), "*", fontsize=14)
+          spl.set_xlabel("Number of components")
+          spl.legend([b[0] for b in bars], cv_types)
+
+          # Plot the winner
+          splot = plt.subplot(2, 1, 2)
+          Y_ = clf.predict(x)
+          for i, (mean, cov, color) in enumerate(zip(clf.means_, clf.covariances_, color_iter)):
+               v, w = linalg.eigh(cov)
+               if not np.any(Y_ == i):
+                    continue
+               plt.scatter(x[Y_ == i, 0], x[Y_ == i, 1], 0.8, color=color)
+
+               # Plot an ellipse to show the Gaussian component
+               angle = np.arctan2(w[0][1], w[0][0])
+               angle = 180.0 * angle / np.pi  # convert to degrees
+               v = 2.0 * np.sqrt(2.0) * np.sqrt(v)
+               ell = mpl.patches.Ellipse(mean, v[0], v[1], 180.0 + angle, color=color)
+               ell.set_clip_box(splot.bbox)
+               ell.set_alpha(0.5)
+               splot.add_artist(ell)
+
+          plt.xticks(())
+          plt.yticks(())
+          plt.title(
+               f"Selected GMM: {best_gmm.covariance_type} model, "
+               f"{best_gmm.n_components} components"
+          )
+          plt.subplots_adjust(hspace=0.35, bottom=0.02)
           plt.show()
+          plt.savefig('BIC score.png')
+        
+    # get evaluation metrics 
+     def evaluate(self, metric_choice):
+        if metric_choice == 0:
+            score = f1_score(self.y_true, self.y_predict, average='macro')
+        elif metric_choice == 1:
+            # IOU score
+            score = jaccard_score(self.y_true, self.y_predict,  average='macro')
+        elif metric_choice == 2:
+            # precision
+            score = precision_score(self.y_true, self.y_predict,  average='macro')
+        elif metric_choice == 3:
+            # recall
+            score = recall_score(self.y_true, self.y_predict, average='macro')
+     
+        return score
+     
+  
+# method for choosing the desired classification metric
+     def classification_metrics(self, actual_ground_truths, predicted_ground_truths):
+        # have predicted and actual ground truth for comparison 
+        self.y_true = actual_ground_truths 
+        self.y_predict = predicted_ground_truths
+
+        print("1",  self.y_true)
+        print("2",  self.y_predict)
+
+
+        score, userInput = "", ""
+        while (userInput != "q"):
+            userInput = input("\nChoose Classification Metric to Evaluate with:"+
+                                    "\n 0 : F1 Score" +
+                                    "\n 1 : Intersection Over Union Score"+
+                                    "\n 2 : Precision"+
+                                    "\n 3 : Recall"+
+                                    "\n 4 : Error Rate"+
+                                    "\n 5 : Confusion matrix"+
+                                    "\n q : Quit\n")
+            if (userInput == "q"): break
+            score = self.evaluate(int(userInput))
+            print(score)
+     
+if __name__ == "__main__":
+    t = Testing()
+    t.classification_metrics()
+
+
      
